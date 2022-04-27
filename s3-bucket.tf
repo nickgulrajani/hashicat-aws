fc0578787cffcb8d22b7a2857c6dcf6a5b0c6f0e
NickG <nicholas.gulrajani@cdw.com>
2022-04-27 21:25:56 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,4 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/nicktfcawslab/s3-bucket/aws"
+  source  = "app.terraform.io/cdw-lab/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-}
