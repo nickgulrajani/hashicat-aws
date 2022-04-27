1fad50d4c9cafb29d6c8fa2fda9229269a26fddb
NickG <nicholas.gulrajani@cdw.com>
2022-04-27 21:04:46 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,9 +2,10 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   acl    = "private"
+  bucket_prefix =  var.prefix
 
   versioning = {
     enabled = true
   }
-bucket_prefix =  $prefix
+
 }
